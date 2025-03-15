9641a513630dfc4c8e993c9f7e3bf290bd1492f5
facunare <arechaga.facundoet36@gmail.com>
2025-03-15 21:18:51 +0000

    terminado

---
+++ build/CMakeFiles/wordle.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wordle.dir/lib.cpp.o"
+  "CMakeFiles/wordle.dir/lib.cpp.o.d"
+  "CMakeFiles/wordle.dir/wordle.cpp.o"
+  "CMakeFiles/wordle.dir/wordle.cpp.o.d"
+  "wordle"
+  "wordle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wordle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/wordleTP
+# Build directory: /workspaces/wordleTP/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/workspaces/wordleTP/build/tests[1]_include.cmake")
+subdirs("_deps/googletest-build")
